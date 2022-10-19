f5b6a2638af22701f2e8e607dc0b5824d6862fcc
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-19 06:12:47 +0530

    Added: Calculate Emp Wage Till Codition

--- src/com/bridgelabz/basics/EmployeeWage.java
+++ src/com/bridgelabz/basics/EmployeeWage.java
@@ -7,26 +7,61 @@ public class EmployeeWage {
 
 
 		int is_Full_Time=1;
+		int is_part_time=0;
 		int wageperHr=20;
 		int FullDayHr=8;
 		int HalfDayHr=4;
-		int DayPerMonth=20;
-		int empcheck = (int)Math.floor(Math.random()*10)%2;
-		if(empcheck==is_Full_Time) {
-			System.out.println("Employee Present ");
-			System.out.println("Employee Daily Wage is="+wageperHr*FullDayHr);
+		int maxHr=100;
+		int maxDays=20;
+		int totalEmpHr=0;
+		int totalWorkingHr=0;
+		int totalWorkingDays=0;
+		int FullTimeDay=0;
+		int absent=0;
+		int partTimeDay=0;
+
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if(empCheck==is_Full_Time) {
+			System.out.println("Employee is Present FULL Time!!");
+			System.out.println("Employee Daily Wage is="+(wageperHr*FullDayHr));
 		}
-		else {		
+		else {
+			System.out.println("Employee is Present Part Time..");
 			System.out.println("Employee Daily Half_Day Wage is="+(wageperHr*HalfDayHr));
 		}
-		int mycheck=(int) empcheck;
-		switch(mycheck) {
-		case 0:
-			System.out.println("Employee Monthly Half_Day Wage is="+(wageperHr*HalfDayHr)*DayPerMonth);
-			break;
-		case 1:
-			System.out.println("Employee Monthly Full Day Wage is="+(wageperHr*FullDayHr)*DayPerMonth);
-			break;
+
+		///Using switch Case
+		while(totalWorkingHr<maxHr && totalWorkingDays<maxDays ) {
+			totalWorkingDays++;
+			int myCheck=(int)empCheck;       //Type Casting
+			switch(myCheck) {
+			case 0:
+				partTimeDay++;               
+				totalEmpHr=totalEmpHr+HalfDayHr;
+				break;
+			case 1:
+				FullTimeDay++;
+				totalEmpHr=totalEmpHr+(FullDayHr/2);
+				break;
+			default:
+				absent++;
+				break;
+			}			
 		}
+		int full_time_incom=wageperHr*(FullDayHr*FullTimeDay);
+		int part_time_incom=(partTimeDay*(FullDayHr/2)*wageperHr);
+		System.out.println("**************emp wages*********************");
+		System.out.println("Full time Days in month:\t"+FullTimeDay);
+		System.out.println("Part time Days in month:\t"+partTimeDay);
+		System.out.println("Absent Days in month:\t\t"+absent);
+		System.out.println("Total working Days in month:\t"+(FullTimeDay+partTimeDay));
+		System.out.println("--------------------------------------------");
+		System.out.println("Employee Full Time Income="+ full_time_incom);
+		System.out.println("Employee part Time Income="+part_time_incom);
+		System.out.println("Total Salary:\t"+(full_time_incom+part_time_incom));
+		System.out.println("--------------------------------------------");
+
+
 	}
+
 }
