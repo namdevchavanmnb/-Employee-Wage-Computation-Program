612e56e73861a0f4db1638c2607c2cc20bf0e7b6
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-19 05:47:22 +0530

    Added: Part time Employee and wage Calculation

--- src/com/bridgelabz/basics/EmployeeWage.java
+++ src/com/bridgelabz/basics/EmployeeWage.java
@@ -5,16 +5,18 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee Wage Computation Program");
 
-		int empcheck = (int)Math.floor(Math.random()*10)%2;
+
 		int is_Full_Time=1;
 		int wageperHr=20;
 		int FullDayHr=8;
+		int HalfDayHr=4;
+		int empcheck = (int)Math.floor(Math.random()*10)%2;
 		if(empcheck==is_Full_Time) {
 			System.out.println("Employee Present ");
 			System.out.println("Employee Daily Wage is="+wageperHr*FullDayHr);
 		}
-		else {
-			System.out.println("Employee Absent");
+		else {		
+			System.out.println("Employee Daily Half_Day Wage is="+(wageperHr*HalfDayHr));
 		}
 	}
 }
