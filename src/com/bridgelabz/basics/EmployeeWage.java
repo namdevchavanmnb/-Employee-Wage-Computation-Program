0e63bb9291a97fa6723b58acc95aed874d1e1820
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-19 05:36:45 +0530

    Added: Employee Daily Wage Calculation

--- src/com/bridgelabz/basics/EmployeeWage.java
+++ src/com/bridgelabz/basics/EmployeeWage.java
@@ -6,9 +6,12 @@ public class EmployeeWage {
 		System.out.println("Welcome to the Employee Wage Computation Program");
 
 		int empcheck = (int)Math.floor(Math.random()*10)%2;
-		
-		if(empcheck ==1) {
+		int is_Full_Time=1;
+		int wageperHr=20;
+		int FullDayHr=8;
+		if(empcheck==is_Full_Time) {
 			System.out.println("Employee Present ");
+			System.out.println("Employee Daily Wage is="+wageperHr*FullDayHr);
 		}
 		else {
 			System.out.println("Employee Absent");
